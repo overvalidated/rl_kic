dd849db26d60dbbbb607c780523cb7139d56d3a8
German Beyger <gyubeyger@edu.hse.ru>
2022-01-31 12:14:14 +0300

    add folder for transformer model

---
+++ transformer_autoencoder/transformer_autoencoder.py
@@ -0,0 +1,14 @@
+import torch
+import numpy as np
+
+from transformer_autoencoder.transformer_autoencoder_model import TransformerAutoEncoder
+
+"""
+    код, ответственный за обучение модели для сжатия расписаний
+    
+"""
+
+if __name__ == "__main__":
+    # training code here
+    model = TransformerAutoEncoder()
+    
---
+++ transformer_autoencoder/transformer_autoencoder_model.py
@@ -0,0 +1,22 @@
+
+import torch
+from torch import nn, optim
+import numpy as np
+
+"""
+    идея простая
+    нужно сжимать в меньшее пространство расписания людей
+
+    нужно общее представление для всех людей
+    также нужно представление каждого отдельного человека
+
+    нужно разделить 
+
+"""
+
+class TransformerAutoEncoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(x):
+        pass
